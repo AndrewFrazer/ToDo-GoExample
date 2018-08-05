53607469cb09da9c61351c7a76aa21d954927d27
Andrew Frazer <andrewsfrazer@gmail.com>
2018-08-05 22:41:45 +0100

    Created todo list api using https://medium.com/@thedevsaddam/build-restful-api-service-in-golang-using-gin-gonic-framework-85b1a6e176f3

---
+++ Go/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+/*
+Remeber to move MySQL to D drive!
+*/
+var db *gorm.DB
+
+func init() {
+	// open db connection
+	var err error
+	// mysql is db driver, root:xxx is username:password, Todos is db name
+	db, err = gorm.Open("mysql", "root:z6Nr5RD&O7Bs@/Todos?charset=utf8&parseTime=True&loc=Local")
+		if err != nil {
+			panic("failed to connect to database")
+		}
+	db.AutoMigrate(&todoModel{})
+}
+
+func main() {
+	router := gin.Default()
+
+	todo := router.Group("/api/1.0/todos")
+	{
+		// add crud routes and methods
+		todo.POST("/", createTodo)
+		todo.GET("/", fetchAllTodo)
+		todo.GET("/:id", fetchTodo)
+		todo.PUT("/:id", updateTodo)
+		todo.DELETE("/:id", deleteTodo)
+	}
+	router.Run()
+}
+
+type (
+	// describes todo model w/ gorm adding ID, CreatedAt, UpdatedAt, DeletedAt
+	todoModel struct {
+		gorm.Model
+		Title		string 	`json:"title"`
+		Completed	int		`json:"completed"`
+	}
+
+	// describes formatted todo
+	transformedTodo struct {
+		ID			uint	`json:"id"`
+		Title		string	`json:"title"`
+		Completed	bool	`json:"completed"`
+	}
+)
+
+// Request body contains todo title and completed bool
+// Create a todo and add it to the db
+// Response is a status code and success message
+func createTodo(c *gin.Context) {
+	// save completed and todo  state
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	todo := todoModel{Title: c.PostForm("title"), Completed: completed}
+	db.Save(&todo)
+	// respond positively
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully", "resourceId": todo.ID})
+}
+
+
+// No request inputs
+// Find and return all todos in db
+// Response is a status code and a list of todos
+func fetchAllTodo(c *gin.Context) {
+	var todos []todoModel
+	var _todos []transformedTodo
+	
+	db.Find(&todos)
+
+	// return NotFound if no todos in db
+	if len(todos) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found!"})
+		return
+	}
+
+	for _, item := range todos {
+		completed := false
+		if item.Completed == 1 {
+			completed = true
+		} else {
+			completed = false
+		}
+
+		_todos = append(_todos, transformedTodo{ID: item.ID, Title: item.Title, Completed: completed})
+	}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todos})
+}
+
+// Request params contains an ID
+// Find and return todo with requested ID
+// Response is a status code and a todo
+func fetchTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Not todo found"})
+		return
+	}
+
+	completed := false
+	if todo.Completed == 1 {
+		completed = true
+	} else {
+		completed = false
+	}
+
+	_todo := transformedTodo{ID: todo.ID, Title: todo.Title, Completed: completed}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": _todo})
+}
+
+// Request params contains an ID, body contains todo title and completed bool
+// Find todo with requested ID and update it with new info
+// Response is a status code and success message
+func updateTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found"})
+		return
+	}
+
+	db.Model(&todo).Update("title", c.PostForm("title"))
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	db.Model(&todo).Update("completed", completed)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo updated successfully"})
+}
+
+// Request params contains an ID
+// Find todo with requested ID and delete it from db
+// Response is a status code and success message
+func deleteTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No todo found"})
+		return
+	}
+
+	db.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": "Todo deleted successfully!"})
+}
